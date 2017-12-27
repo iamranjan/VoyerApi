807fc6f056defce4b67629c7bf0c13d83eced571
Ananda Utama <nanda.utama@team.telstra.com>
2017-12-27 22:55:25 +1100

    changes a the procfile

--- voyerapi/scapi/settings/base.py
+++ voyerapi/scapi/settings/base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #third-party apps
+    # third-party apps
     'corsheaders',
     'rest_framework',
     'rest_framework_swagger',
