3da96861208bcc3243d19852b0cd4cbcf01636b2
Ananda Utama <nanda.utama@team.telstra.com>
2017-12-27 22:33:42 +1100

    comment added

--- voyerapi/scapi/settings/base.py
+++ voyerapi/scapi/settings/base.py
@@ -203,7 +203,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-Simplified static file serving.
-https://warehouse.python.org/project/whitenoise/
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
